07e19c53d84b4355014c38bf49fabf20647703f5
Alexis Moulliere <alexismoulliere44@gmail.com>
2023-11-29 14:57:29 +0100

    mvp

--- affichageGenre.js
+++ affichageGenre.js
@@ -3,6 +3,7 @@ export async function fetchGenreFilm(options) {  //Récupère la liste des genre
       const reponse = await fetch(`https://api.themoviedb.org/3/genre/movie/list?language=en`,options)
       const data = await reponse.json();
       const genreList = data.genres;
+
       return genreList;
     }
     catch (error){
@@ -15,4 +16,18 @@ export function nomGenres(genreArray,genreIdFilm) {  //On map sur le tableau de 
         const genreFound = genreArray.find((objetGenre) => objetGenre.id === idGenre);
         return genreFound ? genreFound.name : null;
     })
+}
+
+export async function fetchTrailer(movie_id, options){
+  try {
+    const reponse = await fetch(`https://api.themoviedb.org/3/movie/${movie_id}/videos`,options)
+    const data = await reponse.json()
+    const trailerMovie = data.results
+    const urlYoutube = `https://www.youtube.com/embed/${trailerMovie[0].key}`
+    console.log(data)
+
+    return  trailerMovie[0].key ? urlYoutube : false;
+  } catch (error) {
+    console.error(error)
+  }
 }
--- horloge.js
+++ horloge.js
@@ -24,6 +24,3 @@ export function hor() {
   document.getElementById('date').textContent = e;
  } 
 
- 
-
-
